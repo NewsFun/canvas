d33155fb95119e2bf71f174c8437e0e60f8e451e
newsfun <80733049@qq.com>
2017-09-29 17:13:34 +0800

    不知道改的啥

--- js/base.js
+++ js/base.js
@@ -1,58 +1,77 @@
-/**
- * Created by bobo on 2017/4/11.
- */
-(function(window, $){
-    var config = {
-        "index":["js/works.js"],
-        "works":["js/works.js"],
-        "course":["js/works.js"],
-        "wc":["js/magnify.js","js/works-content.js"],
-        "cc":["js/course-content.js"]
-    };
-    var pname = $('body').data('name'),
-        names = $('.header .color-grey');
-    var bobo = $('#bo-html'), _text = '', reg = /\{\{((?:.|\n)+?)\}\}/g;
+(function (win,$,undefined) {
+	var w = win.innerWidth;
+	var h = win.innerHeight;
+	var fs = w/10;
+	var moduleList = [];
+	
+	$('html').css('font-size', fs+'px');
 
-    function initPage(){
-        locateName();
-        getBoHtml();
-        jsRely(pname);
+	var testd = {
+		a:'a',
+		b:'b',
+		c:'c',
+		d:'d',
+		e:[{
+			e1:'e1',
+			e2:'e2'
+		}]
+	};
+	win.staticHtml = function(el, data){
+		var mel = $(el), text = '';
+		if(mel.length<1) return false;
+		text = mel.html();
+		mel.html(dataInject(text, data));
+	};
+	function testFun() {
+		var test = $('#test')[0];
+		moduleNode(test, testd);
+	}
+	function dataInject(text, data){
+		var reg = /\{\{((?:.|\n)+?)\}\}/g;
+		while(true){
+			var c = reg.exec(text);
+			if(c === null) return text;
+			text = text.replace(c[0], data[c[1]]);
+		}
+	}
+	function moduleNode(el, data) {
+		var child = el.childNodes;
+		console.log(child);
+ 		if(child.length<1) return;
+ 		for (var i = 0;i<child.length;i++) {
+ 			var outer = child[i].outerHTML;
+ 			if(outer){
+ 				child[i].outerHTML = dataInject(outer, data);
+ 			}
+ 		}
+		/*
+		child.each(function (k,v) {
+			console.log(k,v);
+			moduleIf($(this), data);
+			moduleFor($(this),data);
+		});
+		*/
     }
-    function locateName(){
-        names.each(function(){
-            var href = $(this).attr('href');
-            if(href.indexOf(pname)>-1) $(this).addClass('on');
-        });
-    }
-    function jsRely(name){
-        var js = config[name];
-        if(js){
-            var html = '';
-            for(var i = 0;i<js.length;i++){
-                html += '<script src="'+js[i]+'"></script>';
-            }
-            $('body').append(html);
-        }
-    }
-    function getBoHtml(){
-        if(bobo.length>0) _text = bobo.html();
-        window.TEXT = _text;
-    }
-    window.staticHtml = function(data, node){
-        var text = '', nd = node||bobo;
-        if(bobo.length>0){
-            text = bobo.html();
-            nd.html(dataInject(text, data));
-        }
-    };
-    window.dataInject = function(text, data){
-        //var defaultRegex = "{{(.*?)}}";
-        while(true){
-            var c = reg.exec(text);
-            if(c === null) return text;
-            text = text.replace(c[0], data[c[1]]);
-        }
-    };
-    initPage();
-
+	function moduleIf(el, data) {
+		var mif = el.attr('xh-if');
+		if(!mif||data[mif]){
+			el.removeAttr('xh-if');
+			return;
+		}
+		el.remove();
+	}
+	function moduleFor(el, data) {
+		var mfor = el.attr('xh-for');
+		if(mfor){
+			var dfor = data[mfor];
+			if(dfor&&dfor.length>0){
+				moduleNode(el,dfor);
+			}
+		}else{
+			el.prop("outerHTML", dataInject(el.prop("outerHTML"), data));
+		}
+	}
+	$(win.document).ready(function($) {
+		// testFun();
+	});
 })(window, jQuery);
