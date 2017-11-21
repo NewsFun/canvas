d8ca49a06a490b795b73a0175333ee9afb793fc8
newsfun <80733049@qq.com>
2017-11-21 17:46:31 +0800

    同步测试代码

--- js/cdom.js
+++ js/cdom.js
@@ -1,3 +1,35 @@
 (function(window){
-    var W = window.innerWidth, H = window.innerHeight;
+    const W = window.innerWidth, H = window.innerHeight;
+    const canvas = document.querySelector("canvas");
+    canvas.width = W;
+    canvas.height = H;
+    const ctx = canvas.getContext("2d");
+    const R = Math.random, SIN = Math.sin, COS = Math.cos, PI = Math.PI;
+    class Elf{
+        constructor(param){
+            this.shape = param.shape||'square';
+            this.r = 20;
+            this.color = 'rgba(255,255,255,1)';
+        }
+        render(){
+            this.draw();
+        }
+        draw(){
+            this.shape==='circle'?this.drawCircle():this.drawSquare();
+        }
+        drawCircle(){
+            ctx.beginPath();
+            ctx.fillStyle = this.color;
+            ctx.arc(this.r, this.r, this.r, 0, 2*PI, true);
+            ctx.fill();
+        }
+        drawSquare(){
+            ctx.beginPath();
+            ctx.fillStyle = this.color;
+            ctx.fillRect(0, 0, this.r, this.r);
+            ctx.fill();
+        }
+    }
+    var ef = new Elf({shape:'circle'});
+    ef.render();
 })(window);
