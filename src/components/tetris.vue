1e74101d96e359fa0ad354b7dd28e9288408a90d
newfun <80733049@qq.com>
2018-08-13 23:48:58 +0800

    copy

--- src/components/tetris.vue
+++ src/components/tetris.vue
@@ -1,7 +1,19 @@
 <template></template>
 <script>
+import stage from "@/util/stage.js";
+
+const ctx = stage.context;
+const W = stage.stageWidth;
+const H = stage.stageHeight;
+// 动画
+function animate() {
+  ctx.clearRect(0, 0, W, H);
+  requestAnimationFrame(animate);
+}
 export default {
-  
+  mounted() {
+    animate();
+  }
 }
 </script>
 
