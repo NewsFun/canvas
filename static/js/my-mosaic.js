d1b890176d8fa8918fede2dc2639bea44a470fcb
bobo <80733049@qq.com>
2018-08-14 21:26:01 +0800

    nothing changed

--- static/js/my-mosaic.js
+++ static/js/my-mosaic.js
@@ -1,144 +1,187 @@
 /**
  * Created by bobo on 2017/3/20.
  */
-(function(window){
-    var img = new Image(),
-        canvas = window.document.querySelector('#canvas');
-    img.src = '../img/1.jpg';
-    var w = img.width, h = img.height,
-        ctx = canvas.getContext('2d');
-    var imgData = [], Paths = {}, key = 0, type = {};
-    function mosaic(){
-        canvas.width = w;
-        canvas.height = h;
-        ctx.drawImage(img, 0, 0);
-        imgData = ctx.getImageData(0, 0, w, h).data;
-        new Path().Rectangle({
-            origin:{x:0, y:0},
-            size:{w:w,h:h},
-            monitorType:'mousemove',
-            monitorEvent:evnt
-        });
-    }
-    function evnt(path, pos){
-        //console.log(path);
-        if(path.size.w<2||path.size.h<2) return;
-        if(path.size.w>path.size.h){
-            new Path().Rectangle({
-                origin:path.origin,
-                size:{w:path.size.w/2,h:path.size.h},
-                monitorType:'mousemove',
-                monitorEvent:evnt
-            });
-            new Path().Rectangle({
-                origin:{
-                    x:path.center.x,
-                    y:path.origin.y
-                },
-                size:{w:path.size.w/2,h:path.size.h},
-                monitorType:'mousemove',
-                monitorEvent:evnt
-            });
-        }else{
-            new Path().Rectangle({
-                origin:path.origin,
-                size:{w:path.size.w,h:path.size.h/2},
-                monitorType:'mousemove',
-                monitorEvent:evnt
-            });
-            new Path().Rectangle({
-                origin:{
-                    x:path.origin.x,
-                    y:path.center.y
-                },
-                size:{w:path.size.w,h:path.size.h/2},
-                monitorType:'mousemove',
-                monitorEvent:evnt
-            });
-        }
-        delete Paths[path.key];
-    }
-    function Path(){
-        this.origin = {x:0,y:0};
-        this.size = {w:10,h:10};
-        this.fillColor = 'rgba(255, 255, 255, 0.5)';
-    }
-    Path.prototype = {
-        constructor:Path,
-        Rectangle:function(config){
-            var self = this;
-            mergeObject(self, config).setBounds();
-            self.fillColor = this.getCenterColor();
-            drawRectangle(self);
-            if(self.monitorType) self.monitor();
+(function (window) {
+  var img = new Image(),
+    canvas = window.document.querySelector('#canvas');
+  img.src = '../img/1.jpg';
+  var w = img.width,
+    h = img.height,
+    ctx = canvas.getContext('2d');
+  var imgData = [],
+    Paths = {},
+    key = 0,
+    type = {};
 
-        },
-        monitor:function(){
-            var self = this;
-            self.key = key;
-            Paths[key] = self;
-            if(self.monitorType){
-                if(!type[self.monitorType]){
-                    detector(self.monitorType);
-                    type[self.monitorType] = {};
-                }
-                type[self.monitorType][key] = self;
-            }
+  function mosaic() {
+    canvas.width = w;
+    canvas.height = h;
+    ctx.drawImage(img, 0, 0);
+    imgData = ctx.getImageData(0, 0, w, h).data;
+    new Path().Rectangle({
+      origin: {
+        x: 0,
+        y: 0
+      },
+      size: {
+        w: w,
+        h: h
+      },
+      monitorType: 'mousemove',
+      monitorEvent: evnt
+    });
+  }
 
-            key+=1;
+  function evnt(path, pos) {
+    //console.log(path);
+    if (path.size.w < 2 || path.size.h < 2) return;
+    if (path.size.w > path.size.h) {
+      new Path().Rectangle({
+        origin: path.origin,
+        size: {
+          w: path.size.w / 2,
+          h: path.size.h
         },
-        setBounds:function(){
-            this.maximumx = this.origin.x+this.size.w;
-            this.maximumy = this.origin.y+this.size.h;
-            this.center = {
-                x:Math.ceil(this.origin.x+this.size.w/2),
-                y:Math.ceil(this.origin.y+this.size.h/2)
-            };
-            return this;
+        monitorType: 'mousemove',
+        monitorEvent: evnt
+      });
+      new Path().Rectangle({
+        origin: {
+          x: path.center.x,
+          y: path.origin.y
         },
-        getCenterColor:function(){
-            var x = this.center.x, y = this.center.y;
-            var num = (x+(y-1)*w)*4;
-            var r = imgData[num], g = imgData[num+1], b = imgData[num+2], a = imgData[num+3]/255;
-            //console.log(r, g, b, a);
-            return 'rgba('+r+','+g+','+b+','+a+')';
+        size: {
+          w: path.size.w / 2,
+          h: path.size.h
         },
-        _showCenterPoint:function(){
-            var self = this;
-            drawRectangle({
-                fillColor:'#00ff00',
-                origin:self.center,
-                size:{w:1, h:1}
-            });
-        }
+        monitorType: 'mousemove',
+        monitorEvent: evnt
+      });
+    } else {
+      new Path().Rectangle({
+        origin: path.origin,
+        size: {
+          w: path.size.w,
+          h: path.size.h / 2
+        },
+        monitorType: 'mousemove',
+        monitorEvent: evnt
+      });
+      new Path().Rectangle({
+        origin: {
+          x: path.origin.x,
+          y: path.center.y
+        },
+        size: {
+          w: path.size.w,
+          h: path.size.h / 2
+        },
+        monitorType: 'mousemove',
+        monitorEvent: evnt
+      });
+    }
+    delete Paths[path.key];
+  }
+
+  function Path() {
+    this.origin = {
+      x: 0,
+      y: 0
     };
-    function detector(type){
-        var left = canvas.offsetLeft, top = canvas.offsetTop;
-        canvas.addEventListener(type, function(event){
-            var e = event||window.event;
-            var rx = e.clientX-left, ry = e.clientY-top;
+    this.size = {
+      w: 10,
+      h: 10
+    };
+    this.fillColor = 'rgba(255, 255, 255, 0.5)';
+  }
+  Path.prototype = {
+    constructor: Path,
+    Rectangle: function (config) {
+      var self = this;
+      mergeObject(self, config).setBounds();
+      self.fillColor = this.getCenterColor();
+      drawRectangle(self);
+      if (self.monitorType) self.monitor();
 
-            for(var i in Paths){
-                var p = Paths[i];
-                if( rx>p.origin.x && rx<=p.maximumx && ry>p.origin.y && ry<=p.maximumy ){
-                    if(p.monitorEvent) p.monitorEvent(p, [rx, ry]);
-                }
-            }
-        });
-    }
-    function drawRectangle(path){
-        //ctx.moveTo(config.origin.x, config.origin.y);
-        ctx.save();
-        ctx.fillStyle = path.fillColor;
-        ctx.fillRect(path.origin.x, path.origin.y, path.size.w, path.size.h);
-        ctx.restore();
-        return path;
-    }
-    function mergeObject(result, obj){
-        for(var i in obj){
-            result[i] = obj[i];
+    },
+    monitor: function () {
+      var self = this;
+      self.key = key;
+      Paths[key] = self;
+      if (self.monitorType) {
+        if (!type[self.monitorType]) {
+          detector(self.monitorType);
+          type[self.monitorType] = {};
         }
-        return result;
+        type[self.monitorType][key] = self;
+      }
+
+      key += 1;
+    },
+    setBounds: function () {
+      this.maximumx = this.origin.x + this.size.w;
+      this.maximumy = this.origin.y + this.size.h;
+      this.center = {
+        x: Math.ceil(this.origin.x + this.size.w / 2),
+        y: Math.ceil(this.origin.y + this.size.h / 2)
+      };
+      return this;
+    },
+    getCenterColor: function () {
+      var x = this.center.x,
+        y = this.center.y;
+      var num = (x + (y - 1) * w) * 4;
+      var r = imgData[num],
+        g = imgData[num + 1],
+        b = imgData[num + 2],
+        a = imgData[num + 3] / 255;
+      //console.log(r, g, b, a);
+      return 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
+    },
+    _showCenterPoint: function () {
+      var self = this;
+      drawRectangle({
+        fillColor: '#00ff00',
+        origin: self.center,
+        size: {
+          w: 1,
+          h: 1
+        }
+      });
     }
-    img.onload = mosaic;
+  };
+
+  function detector(type) {
+    var left = canvas.offsetLeft,
+      top = canvas.offsetTop;
+    canvas.addEventListener(type, function (event) {
+      var e = event || window.event;
+      var rx = e.clientX - left,
+        ry = e.clientY - top;
+
+      for (var i in Paths) {
+        var p = Paths[i];
+        if (rx > p.origin.x && rx <= p.maximumx && ry > p.origin.y && ry <= p.maximumy) {
+          if (p.monitorEvent) p.monitorEvent(p, [rx, ry]);
+        }
+      }
+    });
+  }
+
+  function drawRectangle(path) {
+    //ctx.moveTo(config.origin.x, config.origin.y);
+    ctx.save();
+    ctx.fillStyle = path.fillColor;
+    ctx.fillRect(path.origin.x, path.origin.y, path.size.w, path.size.h);
+    ctx.restore();
+    return path;
+  }
+
+  function mergeObject(result, obj) {
+    for (var i in obj) {
+      result[i] = obj[i];
+    }
+    return result;
+  }
+  img.onload = mosaic;
 })(window);
--- static/js/news-index.js
+++ static/js/news-index.js
@@ -4,159 +4,182 @@
 var canvas = document.querySelector('#canvas');
 var ctx = canvas.getContext('2d');
 var W = window.innerWidth,
-    H = window.innerHeight,
-    half_W = W/2, half_H = H/2, clock = 0,
-    R = Math.random,
-    MAX = Math.max,
-    MIN = Math.min,
-    SIN = Math.sin,
-    COS = Math.cos,
-    RAD = Math.PI/180;
-//var ys = randomY(5);
-//var ys = [0,1000,500,-1000,2000];
+  H = window.innerHeight,
+  half_W = W / 2,
+  half_H = H / 2,
+  clock = 0,
+  R = Math.random,
+  MAX = Math.max,
+  MIN = Math.min,
+  SIN = Math.sin,
+  COS = Math.cos,
+  RAD = Math.PI / 180;
 var ys = 0;
-function initCanvas(){
-    canvas.width = W; canvas.height = H;
-    ctx.strokeStyle = 'white';
-    ctx.fillStyle = 'white';
-    ctx.lineWidth = 1;
-}
-function EllipseTwo(context, x, y, a, b) {
-    context.save();
-    var r = (a > b) ? a : b;
-    var ratioX = a / r;
-    var ratioY = b / r;
-    context.scale(ratioX, ratioY);
-    context.beginPath();
-    context.arc(x / ratioX, y / ratioY, r, 0, 2*Math.PI, false);
-    context.closePath();
-    context.restore();
-    //context.fill();
-    context.stroke();
+
+function initCanvas() {
+  canvas.width = W;
+  canvas.height = H;
+  ctx.strokeStyle = 'white';
+  ctx.fillStyle = 'white';
+  ctx.lineWidth = 1;
 }
 
-function drawCurve(start, cross, end){
-    ctx.save();
-    ctx.beginPath();
-    ctx.moveTo(start.x,start.y);
-    ctx.quadraticCurveTo(cross.x, cross.y, end.x, end.y);
-    ctx.restore();
-    ctx.stroke();
+function EllipseTwo(context, x, y, a, b) {
+  context.save();
+  var r = (a > b) ? a : b;
+  var ratioX = a / r;
+  var ratioY = b / r;
+  context.scale(ratioX, ratioY);
+  context.beginPath();
+  context.arc(x / ratioX, y / ratioY, r, 0, 2 * Math.PI, false);
+  context.closePath();
+  context.restore();
+  //context.fill();
+  context.stroke();
 }
-function renderPoint(array){
-    for(var i = 0;i<array.length;i++){
-        drawPoint(array[i]);
-    }
+
+function drawCurve(start, cross, end) {
+  ctx.save();
+  ctx.beginPath();
+  ctx.moveTo(start.x, start.y);
+  ctx.quadraticCurveTo(cross.x, cross.y, end.x, end.y);
+  ctx.restore();
+  ctx.stroke();
 }
-function drawPoint(point){
-    ctx.fillStyle = 'white';
-    ctx.beginPath();
-    ctx.arc(point.x, point.y, 10, 0, 2*Math.PI);
-    ctx.fill();
+
+function renderPoint(array) {
+  for (var i = 0; i < array.length; i++) {
+    drawPoint(array[i]);
+  }
 }
-function _point(x, y){
-    this.x = x;
-    this.y = y;
+
+function drawPoint(point) {
+  ctx.fillStyle = 'white';
+  ctx.beginPath();
+  ctx.arc(point.x, point.y, 10, 0, 2 * Math.PI);
+  ctx.fill();
 }
-function _circlePoint(rot){
-    this.center = {x:half_W, y:half_H};
-    this.rot = rot;
-    this.dis = 400;
-    this.a = 3;
-    this.b = 0.2;
-    //this.rad = 0;
-    this.x = COS((clock-rot)*RAD)*this.dis*this.a+this.center.x;
-    this.y = SIN((clock-rot)*RAD)*this.dis*this.b+this.center.y;
+
+function _point(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+function _circlePoint(rot) {
+  this.center = {
+    x: half_W,
+    y: half_H
+  };
+  this.rot = rot;
+  this.dis = 400;
+  this.a = 3;
+  this.b = 0.2;
+  //this.rad = 0;
+  this.x = COS((clock - rot) * RAD) * this.dis * this.a + this.center.x;
+  this.y = SIN((clock - rot) * RAD) * this.dis * this.b + this.center.y;
 }
 _circlePoint.prototype = {
-    moveTo:function(point){
-        var self = this;
-        var dis = self.distance(point, true);
-        var d = dis[2], dx = dis[0], dy = dis[1];
-        if(d>1){
-            this.x -= dx*this.g;
-            this.y -= dy*this.g;
-        }
-    },
-    distance:function(n, details){
-        var dx = this.x - n.x,
-            dy = this.y - n.y,
-            d = Math.sqrt(dx * dx + dy * dy);
+  moveTo: function (point) {
+    var self = this;
+    var dis = self.distance(point, true);
+    var d = dis[2],
+      dx = dis[0],
+      dy = dis[1];
+    if (d > 1) {
+      this.x -= dx * this.g;
+      this.y -= dy * this.g;
+    }
+  },
+  distance: function (n, details) {
+    var dx = this.x - n.x,
+      dy = this.y - n.y,
+      d = Math.sqrt(dx * dx + dy * dy);
 
-        return details ? [dx, dy, d] : d;
-    },
-    move:function(){
-        var self = this;
-        self.x = COS((clock*6-self.rot)*RAD)*this.dis*this.a+this.center.x;
-        self.y = SIN((clock*6-self.rot)*RAD)*this.dis*this.b+this.center.y;
-        self.b = SIN(clock*RAD)/4;
-        if(self.a>1) self.a *= 0.985;
-    }
+    return details ? [dx, dy, d] : d;
+  },
+  move: function () {
+    var self = this;
+    self.x = COS((clock * 6 - self.rot) * RAD) * this.dis * this.a + this.center.x;
+    self.y = SIN((clock * 6 - self.rot) * RAD) * this.dis * this.b + this.center.y;
+    self.b = SIN(clock * RAD) / 4;
+    if (self.a > 1) self.a *= 0.985;
+  }
 };
-function endPoint(x, y, r, R, rot){
-    var list_s = [], list_l = [];
-    for(var i = 0;i<5;i++){
-        list_s.push(new _point(COS((i*72-rot+54)*RAD)*R+x, -SIN((i*72-rot+54)*RAD)*R+y));
-        list_l.push(new _point(COS((i*72-rot+18)*RAD)*r+x, -SIN((i*72-rot+18)*RAD)*r+y));
-    }
-    return {
-        outerPoint:list_l,
-        innerPoint:list_s
-    }
+
+function endPoint(x, y, r, R, rot) {
+  var list_s = [],
+    list_l = [];
+  for (var i = 0; i < 5; i++) {
+    list_s.push(new _point(COS((i * 72 - rot + 54) * RAD) * R + x, -SIN((i * 72 - rot + 54) * RAD) * R + y));
+    list_l.push(new _point(COS((i * 72 - rot + 18) * RAD) * r + x, -SIN((i * 72 - rot + 18) * RAD) * r + y));
+  }
+  return {
+    outerPoint: list_l,
+    innerPoint: list_s
+  }
 }
-function drawLine(pointList, crossList){
-    for(var i = 0;i<pointList.length;i++){
-        var j = (i+1)%pointList.length;
-        drawCurve(pointList[i], crossList[i], pointList[j]);
-    }
+
+function drawLine(pointList, crossList) {
+  for (var i = 0; i < pointList.length; i++) {
+    var j = (i + 1) % pointList.length;
+    drawCurve(pointList[i], crossList[i], pointList[j]);
+  }
 }
-function createCrossPoint(array){
-    /*var y = ys[0];*/
-    var list = [], y = [];
-    for(var i = 0;i<array.length;i++){
-        var j = (i+1)%array.length;
-        var x = (array[j].x-array[i].x)*R()+array[i].x;
-        //var p = new _point(x, y);
-        y =
-        list.push(new _point(x, ys[i]));
-    }
-    return list;
+
+function createCrossPoint(array) {
+  /*var y = ys[0];*/
+  var list = [],
+    y = [];
+  for (var i = 0; i < array.length; i++) {
+    var j = (i + 1) % array.length;
+    var x = (array[j].x - array[i].x) * R() + array[i].x;
+    //var p = new _point(x, y);
+    y =
+      list.push(new _point(x, ys[i]));
+  }
+  return list;
 }
-function randomY(n){
-    var list = [];
-    for(var i = 0;i<n;i++){
-        list.push(~~(R()*2*H));
-    }
-    return list;
+
+function randomY(n) {
+  var list = [];
+  for (var i = 0; i < n; i++) {
+    list.push(~~(R() * 2 * H));
+  }
+  return list;
 }
-function createClickPoint(n){
-    var num = n|| 5, rad = ~~(360/num), list = [];
-    for(var i = 0;i<num;i++){
-        list.push(new _circlePoint(rad*i));
-    }
-    return list;
+
+function createClickPoint(n) {
+  var num = n || 5,
+    rad = ~~(360 / num),
+    list = [];
+  for (var i = 0; i < num; i++) {
+    list.push(new _circlePoint(rad * i));
+  }
+  return list;
 }
-function update(clickPoint){
-    for(var i = 0;i<clickPoint.length;i++){
-        clickPoint[i].move();
-        ys[i] -= (ys[i]-half_H)*0.02;
-    }
-    var cross = createCrossPoint(clickPoint);
-    drawLine(clickPoint, cross);
-    renderPoint(clickPoint);
+
+function update(clickPoint) {
+  for (var i = 0; i < clickPoint.length; i++) {
+    clickPoint[i].move();
+    ys[i] -= (ys[i] - half_H) * 0.02;
+  }
+  var cross = createCrossPoint(clickPoint);
+  drawLine(clickPoint, cross);
+  renderPoint(clickPoint);
 }
 /*test point*/
 var testlist = createClickPoint();
 var endpoint = endPoint(half_W, half_H);
-function animate(){
-    ctx.clearRect(0, 0, W, H);
-    //update(click_point, cross_point, click_end, cross_end);
-    //testpoint.move();
-    //drawPoint(testpoint);
-    //renderPoint(testlist);
-    update(testlist);
-    clock++;
-    if(clock<100)requestAnimationFrame(animate);
+
+function animate() {
+  ctx.clearRect(0, 0, W, H);
+  //update(click_point, cross_point, click_end, cross_end);
+  //testpoint.move();
+  //drawPoint(testpoint);
+  //renderPoint(testlist);
+  update(testlist);
+  clock++;
+  if (clock < 100) requestAnimationFrame(animate);
 }
 initCanvas();
 animate();
