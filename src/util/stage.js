4f2716e83a49c37e9e2fd492b47adc94e367c427
bobo <80733049@qq.com>
2018-08-28 15:15:53 +0800

    add: cache-canvas

--- src/util/stage.js
+++ src/util/stage.js
@@ -1,7 +1,13 @@
-export let Stage = document.getElementById('canvas')
-export let ctx = Stage.getContext('2d')
-export let W = window.innerWidth
-export let H = window.innerHeight
+export const Stage = document.getElementById('canvas')
+export const Cache = document.getElementById('cache')
+export const cachectx = Cache.getContext('2d')
+export const ctx = Stage.getContext('2d')
+export const H = window.innerHeight
+export const W = window.innerWidth
+
+Cache.style.display = 'none'
+Cache.width = W
+Cache.height = H
 
 Stage.width = W
 Stage.height = H
