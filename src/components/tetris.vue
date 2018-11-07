db0f8c1e36ec71bfc79b3c5461d98d26dff70e29
bobo <80733049@qq.com>
2018-11-07 16:48:05 +0800

    change render 0.5

--- src/components/tetris.vue
+++ src/components/tetris.vue
@@ -7,6 +7,9 @@ import { pixelStorage } from "@/util/tetris.js";
 import { randomInteger } from "@/util/tools.js";
 import { render } from "@/util/render.js";
 
+const MIN = Math.min;
+const MAX = Math.max;
+
 const W = 400;
 const H = 800;
 const VX = 20;
@@ -14,13 +17,16 @@ const cachLen = VX * 4;
 const COLORS = ["#409EFF", "#67C23A", "#E6A23C", "#F56C6C"];
 
 let vy = 1;
+let pixels = null;
+let walls = [];
+let renderList = [];
 
 class Pixels {
   constructor(ctx) {
-    this.type = [];
+    this.bound = [0, 0, 0, 0];
     this.index = 0;
     this.c = randomColor();
-    this.ctx = ctx;
+    this.type = getPixelType();
     this.list = this.type2pixel();
   }
   update(dist = 0) {
@@ -28,11 +34,8 @@ class Pixels {
       e.y += vy;
       e.x += dist;
     });
-    return this;
   }
   type2pixel() {
-    this.type = creatPixel();
-
     let tlist = this.type[this.index];
     let plist = [];
 
@@ -46,20 +49,32 @@ class Pixels {
         type: "rect"
       });
     });
+
     return plist;
+  }
+  checkBound() {
+    let bx = [];
+    let by = [];
+
+    this.list.forEach(e => {
+      bx.push(e.x);
+      by.push(e.y);
+    });
+
+    let minx = MIN(...bx);
+    let miny = MIN(...by);
+    let maxx = MAX(...bx);
+    let maxy = MAX(...by);
+
+    return [miny, maxx + VX, maxy + VX, minx];
   }
   rotate() {
     let len = this.type.length;
     this.index = (this.index + 1) % len;
   }
-  render() {
-    this.list.forEach(e => {
-      render(this.ctx, e);
-    });
-  }
 }
 
-function creatPixel() {
+function getPixelType() {
   let len = pixelStorage.length;
   let ind = randomInteger(len);
   return pixelStorage[ind];
@@ -71,8 +86,10 @@ function randomColor() {
   return COLORS[ind];
 }
 
-cacheCtx.width = cachLen;
-cacheCtx.height = cachLen;
+function createPixels() {
+  pixels = new Pixels();
+  renderList = renderList.concat(pixels.list);
+}
 
 export default {
   computed: {
@@ -84,19 +101,32 @@ export default {
     },
     ctx() {
       return this.Stage.getContext("2d");
-    },
-    pixels() {
-      return new Pixels(this.ctx);
     }
   },
   mounted() {
+    createPixels();
     this.animate();
   },
   methods: {
     animate() {
       this.ctx.clearRect(0, 0, W, H);
-      this.pixels.update().render();
+      this.update();
       requestAnimationFrame(this.animate);
+    },
+    update() {
+      let b = pixels.checkBound()[2];
+      if (b < H) {
+        pixels.update();
+      } else {
+        walls = walls.concat(pixels.list);
+        createPixels();
+      }
+      this.render();
+    },
+    render() {
+      renderList.forEach(e => {
+        render(this.ctx, e);
+      });
     }
   }
 };
